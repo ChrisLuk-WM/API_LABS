84db29b4ddc8c38513a74b35730250aa7edb1831
ChrisLuk-WM <lukchinpang@hotmail.com>
2025-02-19 19:45:07 +0800

    initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import Koa from 'koa';
+import Router, { RouterContext } from 'koa-router';
+import logger from 'koa-logger';
+import json from 'koa-json';
+import bodyParser from 'koa-bodyparser';
+import { CustomErrorMessageFunction, query, body, validationResults } 
+  from 'koa-req-validation';
+
+const app: Koa = new Koa();
+const router: Router = new Router();
+const welcomeAPI = async (ctx: RouterContext, next: any) => {
+    ctx.body = {
+        message: "Welcome to the blog API!"
+    };
+    await next();
+   }
+
+router.get('/api/v1', welcomeAPI);
+app.use(logger());
+app.use(json());
+app.use(router.routes());
+app.listen(10888);
