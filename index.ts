4bbe4e5163ddfd48665b58889df7fc2e4b2ae6be
ChrisLuk-WM <lukchinpang@hotmail.com>
2025-02-19 21:08:21 +0800

    lab5

--- index.ts
+++ index.ts
@@ -1,22 +1,26 @@
 import Koa from 'koa';
-import Router, { RouterContext } from 'koa-router';
+// import Router, { RouterContext } from 'koa-router';
 import logger from 'koa-logger';
 import json from 'koa-json';
-import bodyParser from 'koa-bodyparser';
-import { CustomErrorMessageFunction, query, body, validationResults } 
-  from 'koa-req-validation';
+// import bodyParser from 'koa-bodyparser';
+// import { CustomErrorMessageFunction, query, body, validationResults } 
+//   from 'koa-req-validation';
+import { router as Articles } from './routers/articles';
 
 const app: Koa = new Koa();
-const router: Router = new Router();
-const welcomeAPI = async (ctx: RouterContext, next: any) => {
-    ctx.body = {
-        message: "Welcome to the blog API!"
-    };
-    await next();
-   }
+// const router: Router = new Router();
+// const welcomeAPI = async (ctx: RouterContext, next: any) => {
+//     ctx.body = {
+//         message: "Welcome to the blog API!"
+//     };
+//     await next();
+// }
 
-router.get('/api/v1', welcomeAPI);
+
+// router.get('/api/v1', welcomeAPI);
+
 app.use(logger());
 app.use(json());
-app.use(router.routes());
+app.use(Articles.routes())
+// app.use(router.routes());
 app.listen(10888);
